39ce2fc42a75b1f9829a261208867369670fdafa
sohail60 <sohailatwork10@gmail.com>
2023-05-22 17:21:13 +0530

    Carousel Removed

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,66 +1,101 @@
-import react,{useState} from 'react';
-import Carousel from "react-elastic-carousel";
-import './Project.css'
-import Card from '../subcomponents/Card/Card'
-import Item from "./Item";
+import react, { useState } from "react";
+// import { Carousel } from "react-responsive-carousel";
+// import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "./Project.css";
+import Card from "../subcomponents/Card/Card";
 
-function Project(){
-    const [mega,setMega]=useState(true);
-    const [pet,setPet]=useState(false);
+function Project() {
+  const [mega, setMega] = useState(true);
+  const [pet, setPet] = useState(false);
 
-    function handleMega(e){
-        setMega(true);
-        setPet(false);
-    }
+  function handleMega(e) {
+    setMega(true);
+    setPet(false);
+  }
 
-    function handlePet(e){
-        setPet(true);
-        setMega(false);
-    }
+  function handlePet(e) {
+    setPet(true);
+    setMega(false);
+  }
 
-    const breakPoints = [
-        { width: 1, itemsToShow: 1 },
-        { width: 550, itemsToShow: 2 },
-        { width: 768, itemsToShow: 3 },
-        { width: 1200, itemsToShow: 4 },
-      ];
+  return (
+    <div id="project-container">
+      <h2>Project</h2>
+      <div id="project-btn-container">
+        <button className="project-btn" onClick={handleMega}>
+          Mega Projects
+        </button>
+        <button className="project-btn" onClick={handlePet}>
+          Pet Projects
+        </button>
+      </div>
 
-    return(
-        <div id="project-container">
-            <h2>Project</h2>
-            <div id="project-btn-container">
-            <button className='project-btn' onClick={handleMega}>Mega Projects</button>
-            <button className='project-btn' onClick={handlePet}>Pet Projects</button>
-            </div>
+      <div id="mega-project" className="project">
+        {mega && (
+          <>
+              <Card
+                id="color-game-card"
+                className="mega-project-card"
+                link="https://sohail60.github.io/color-guesser-game/"
+                imageLink="https://drive.google.com/file/d/1FK5owhn66Yyqx4hVHlp-HSyvIb9nNzLY/view?usp=share_link"
+                alt="color-game-project.png"
+              />
 
-            <div id="mega-project" className='project'>
-            {mega && (
-            <>
-            
-            <Card id='color-game-card' className='mega-project-card' link="https://sohail60.github.io/color-guesser-game/" imageLink='https://drive.google.com/file/d/1FK5owhn66Yyqx4hVHlp-HSyvIb9nNzLY/view?usp=share_link' alt='color-game-project.png'/>
+              {/* CHANGES HERE IF MADE NEW PROJECT TO LIST IN THE PORTFOLIO */}
+              <Card
+                id="color-game-card"
+                className="mega-project-card"
+                link="https://sohail60.github.io/color-guesser-game/"
+                imageLink="../../images/color-game-project.png"
+                alt="color-game-project.png"
+              />
+          </>
+        )}
+      </div>
 
-            {/* /* CHANGES HERE IF MADE NEW PROJECT TO LIST IN THE PORTFOLIO */}
-            <Card id='color-game-card' className='mega-project-card' link="https://sohail60.github.io/color-guesser-game/" imageLink='../../images/color-game-project.png' alt='color-game-project.png'/>
-            </>
-            )}
-            </div>
-            
-            <div id="pet-project" className='project'>
+      <div id="pet-project" className="project">
+        {pet && (
+          <>
+              <Card
+                id="color-game-card"
+                className="pet-project-card"
+                link="https://sohail60.github.io/color-guesser-game/"
+                imageLink="../../images/color-game-project.png"
+                alt="color-game-project.png"
+              />
+              <Card
+                id="dice-game-card"
+                className="pet-project-card"
+                link="https://sohail60.github.io/dice-game/"
+                imageLink="../../images/dice-game-project.png"
+                alt="dice-game-project.png"
+              />
+              <Card
+                id="ping-pong-card"
+                className="pet-project-card"
+                link="https://sohail60.github.io/ping-pong-score-keeper/"
+                imageLink="../../images/ping-pong-project.png"
+                alt="color-game-project.png"
+              />
+              <Card
+                id="todo-list-card"
+                className="pet-project-card"
+                link="https://sohail60.github.io/To-Do-List/"
+                imageLink="../../images/todo-list-project.png"
+                alt="color-game-project.png"
+              />
 
-            {pet && (
-                <>
-                <Carousel breakPoints={breakPoints}>
+            {/* <Carousel breakPoints={breakPoints}>
                 <Item><Card id='color-game-card' className='pet-project-card' link="https://sohail60.github.io/color-guesser-game/" imageLink='../../images/color-game-project.png' alt='color-game-project.png'/></Item>
                 <Item><Card id='dice-game-card' className='pet-project-card' link="https://sohail60.github.io/dice-game/" imageLink='../../images/dice-game-project.png' alt='dice-game-project.png'/></Item>
                 <Item><Card id='ping-pong-card' className='pet-project-card' link="https://sohail60.github.io/ping-pong-score-keeper/" imageLink='../../images/ping-pong-project.png' alt='color-game-project.png'/></Item>
                 <Item><Card id='todo-list-card' className='pet-project-card' link="https://sohail60.github.io/To-Do-List/" imageLink='../../images/todo-list-project.png' alt='color-game-project.png'/></Item>
-                </Carousel>
-                </>
-            )}
-            
-            </div>
-            </div>
-    );
+                </Carousel> */}
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Project;
--- src/components/subcomponents/Card/Card.jsx
+++ src/components/subcomponents/Card/Card.jsx
@@ -3,14 +3,15 @@ import colorgame from '../../images/color-game-project.png'
 import dicegame from '../../images/dice-game-project.png'
 import pingpong from '../../images/ping-pong-project.png'
 import todolist from '../../images/todo-list-project.png'
+import project from '../../images/project.png'
 
 function Card(props){
     return(
         <div id={props.id} className={props.className}>
             <a href={props.link}>
             {/* <img src={require('{props.imageLink}')} alt="Hero png"/> */}
-            <img src={props.imageLink} alt={props.alt}/>
-            {/* <img src={dicegame} alt={props.alt}/> */}
+            {/* <img src={props.imageLink} alt={props.alt}/> */}
+            <img src={project} alt={props.alt}/>
             {/* <img src={require('../../images/hero.png')} alt="Hero png"/> */}
             {/* ../images/color-game-project.png */}
             </a>
